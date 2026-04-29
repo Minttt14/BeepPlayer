0bb843bc29d9ca1f822bf88b4aa60566d290382c
Minttt14 <Xuan.cyo26@gmail.com>
2026-04-30 01:08:38 +0800

    模擬鋼琴鍵的外觀

--- BeepPlayer/frmBeepPlayer.Designer.cs
+++ BeepPlayer/frmBeepPlayer.Designer.cs
@@ -37,11 +37,22 @@
             this.btn3 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
             this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.palMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // palMain
             // 
+            this.palMain.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.palMain.Controls.Add(this.label5);
+            this.palMain.Controls.Add(this.label4);
+            this.palMain.Controls.Add(this.label3);
+            this.palMain.Controls.Add(this.label2);
+            this.palMain.Controls.Add(this.label1);
             this.palMain.Controls.Add(this.btn8);
             this.palMain.Controls.Add(this.btn7);
             this.palMain.Controls.Add(this.btn6);
@@ -53,87 +64,156 @@
             this.palMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.palMain.Location = new System.Drawing.Point(0, 0);
             this.palMain.Name = "palMain";
-            this.palMain.Size = new System.Drawing.Size(920, 186);
+            this.palMain.Size = new System.Drawing.Size(901, 188);
             this.palMain.TabIndex = 0;
             // 
             // btn8
             // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn8.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn8.ForeColor = System.Drawing.Color.Red;
-            this.btn8.Location = new System.Drawing.Point(799, 38);
+            this.btn8.Location = new System.Drawing.Point(741, 0);
             this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(86, 112);
+            this.btn8.Size = new System.Drawing.Size(100, 188);
             this.btn8.TabIndex = 7;
             this.btn8.Text = "Do";
-            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn8.UseVisualStyleBackColor = false;
             // 
             // btn7
             // 
-            this.btn7.Location = new System.Drawing.Point(689, 38);
+            this.btn7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn7.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn7.Location = new System.Drawing.Point(644, 0);
             this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(86, 112);
+            this.btn7.Size = new System.Drawing.Size(100, 188);
             this.btn7.TabIndex = 6;
             this.btn7.Text = "Si";
-            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn7.UseVisualStyleBackColor = false;
             // 
             // btn6
             // 
-            this.btn6.Location = new System.Drawing.Point(576, 38);
+            this.btn6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn6.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn6.Location = new System.Drawing.Point(546, 0);
             this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(86, 112);
+            this.btn6.Size = new System.Drawing.Size(100, 188);
             this.btn6.TabIndex = 5;
             this.btn6.Text = "La";
-            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn6.UseVisualStyleBackColor = false;
             // 
             // btn5
             // 
-            this.btn5.Location = new System.Drawing.Point(463, 38);
+            this.btn5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn5.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn5.Location = new System.Drawing.Point(449, 0);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(86, 112);
+            this.btn5.Size = new System.Drawing.Size(100, 188);
             this.btn5.TabIndex = 4;
             this.btn5.Text = "Sol";
-            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn5.UseVisualStyleBackColor = false;
             // 
             // btn4
             // 
-            this.btn4.Location = new System.Drawing.Point(351, 38);
+            this.btn4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn4.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn4.Location = new System.Drawing.Point(351, 0);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(86, 112);
+            this.btn4.Size = new System.Drawing.Size(100, 188);
             this.btn4.TabIndex = 3;
             this.btn4.Text = "Fa";
-            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn4.UseVisualStyleBackColor = false;
             // 
             // btn3
             // 
-            this.btn3.Location = new System.Drawing.Point(243, 38);
+            this.btn3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn3.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn3.Location = new System.Drawing.Point(254, 0);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(86, 112);
+            this.btn3.Size = new System.Drawing.Size(100, 188);
             this.btn3.TabIndex = 2;
             this.btn3.Text = "Mi";
-            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn3.UseVisualStyleBackColor = false;
             // 
             // btn2
             // 
-            this.btn2.Location = new System.Drawing.Point(136, 38);
+            this.btn2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn2.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(156, 0);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(86, 112);
+            this.btn2.Size = new System.Drawing.Size(100, 188);
             this.btn2.TabIndex = 1;
             this.btn2.Text = "Re";
-            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn2.UseVisualStyleBackColor = false;
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(29, 38);
+            this.btn1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn1.Font = new System.Drawing.Font("微軟正黑體", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(58, 0);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(86, 112);
+            this.btn1.Size = new System.Drawing.Size(100, 188);
             this.btn1.TabIndex = 0;
             this.btn1.Text = "Do";
-            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(138, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 115);
+            this.label1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(236, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 115);
+            this.label2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(432, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 115);
+            this.label3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(529, 2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 115);
+            this.label4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(626, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 115);
+            this.label5.TabIndex = 13;
             // 
             // frmBeepPlayer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(920, 186);
+            this.ClientSize = new System.Drawing.Size(901, 188);
             this.Controls.Add(this.palMain);
             this.Name = "frmBeepPlayer";
             this.Text = "簡易電子琴";
@@ -155,6 +235,11 @@
         private System.Windows.Forms.Button btn3;
         private System.Windows.Forms.Button btn2;
         private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
